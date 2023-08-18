45ed8feb275f0f505fe6268e93a58a4914239a69
yduman <yusufdumans61@gmail.com>
2023-08-18 12:01:44 +0300

    New Assets

--- Zigzag/Assets/Scripts/Dedectors/DiamondDedector.cs
+++ Zigzag/Assets/Scripts/Dedectors/DiamondDedector.cs
@@ -4,12 +4,14 @@ public class DiamondDedector : MonoBehaviour
 {
     [SerializeField] int diamondValue; 
     [SerializeField] ScoreManager scoreManager;
+    [SerializeField] AudioSource collectSound;
     [SerializeField] DiamondController diamondController;
 
     void OnTriggerEnter2D(Collider2D collider){
         if(collider.gameObject.CompareTag("diamond")){
             scoreManager.IncreaseScore(diamondValue);
             diamondController.RepositionTheText(collider.transform.position); // Display the extra point text/animation
+            collectSound.Play();
             collider.gameObject.SetActive(false);
         }
     }
